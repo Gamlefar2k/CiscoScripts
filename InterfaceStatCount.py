3c67385e29739b356ec59207898d511803f71eb5
Gamlefar2k <119758627+Gamlefar2k@users.noreply.github.com>
2024-06-16 22:06:24 +0000

    Update InterfaceStatCount.py

--- InterfaceStatCount.py
+++ InterfaceStatCount.py
@@ -1,1 +1,13 @@
+import csv
+import re
 
+commands = (
+  "show int status | inc Gi|Te|Tw|Po",
+  "show int count | inc Gi|Te|Tw|Po"
+)
+
+def main():
+  data = {}
+
+crt.GetScriptTab().Screen.Send("\r")
+hostname = crt.GetScriptTab().Screen.ReadString("#")
